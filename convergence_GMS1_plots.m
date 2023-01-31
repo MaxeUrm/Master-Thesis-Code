624f62a706bfb6ed7dc0216fa663417f8a9a66d4
Maxmilian Urmann <72104832+MaxeUrm@users.noreply.github.com>
2023-01-31 15:21:03 +0100

    Add files via upload

--- convergence_GMS1_plots.m
+++ convergence_GMS1_plots.m
@@ -35,7 +35,7 @@ u_gamma = @(x,t) exp(-t);
 
 %% Discretization sizes
 
-h_vec = [0.15; 0.1; 0.07; 0.05; 0.035]; % doubling the dof
+h_vec = [0.15; 0.1; 0.07; 0.05; 0.035; 0.025]; % doubling the dof
 
 % dofs
 % 0.1 -> 362, 0.07 -> 737, 0.05 -> 1452, 0.035 -> 2955, 0.025 -> 5809,
@@ -43,16 +43,16 @@ h_vec = [0.15; 0.1; 0.07; 0.05; 0.035]; % doubling the dof
 
 tau_vec = [10; 20; 40; 80; 160; 320; 640; 1280]; % doubling the number of timesteps
 
-h_arr = zeros(5,1);
+h_arr = zeros(6,1);
 tau_arr = zeros(8,1);
 
-L2_err_matrix = zeros(5,8);
-H1_err_matrix = zeros(5,8);
+L2_err_matrix = zeros(6,8);
+H1_err_matrix = zeros(6,8);
 
 %% Compute errors
 %
 
-for i = 1:5
+for i = 1:6
     for j = 1:8
         i
         j
@@ -66,9 +66,7 @@ for i = 1:5
     end
 end
 %}
-
 %% Plotting space
-%
 
 loyolagreen = 1/255*[0,104,87];
 deepcarrotorange = [0.9100, 0.4100, 0.1700];
@@ -85,43 +83,52 @@ set(0, 'defaultAxesTickLabelInterpreter','latex');
 set(0, 'defaultLegendInterpreter','latex');
 
 figure(1) 
-subplot(1,2,1)
-loglog(h_arr,L2_err_matrix(:,1),"-o",'LineWidth',2)
+subplot(2,2,1)
+loglog(h_arr,L2_err_matrix(:,1),"-o",'LineWidth',2, 'Markersize', 10)
 hold on
-loglog(h_arr,L2_err_matrix(:,2),"-*",'LineWidth',2)
-loglog(h_arr,L2_err_matrix(:,3),"-+",'LineWidth',2)
-loglog(h_arr,L2_err_matrix(:,4),"-x",'LineWidth',2)
-loglog(h_arr,L2_err_matrix(:,5),"-.",'LineWidth',2)
-loglog(h_arr,L2_err_matrix(:,6),"-diamond",'LineWidth',2)
-loglog(h_arr,L2_err_matrix(:,7),"-square",'LineWidth',2)
-loglog(h_arr,L2_err_matrix(:,8),"-^",'LineWidth',2)
+loglog(h_arr,L2_err_matrix(:,2),"-*",'LineWidth',2, 'Markersize', 10)
+loglog(h_arr,L2_err_matrix(:,3),"-+",'LineWidth',2, 'Markersize', 10)
+loglog(h_arr,L2_err_matrix(:,4),"-x",'LineWidth',2, 'Markersize', 10)
+loglog(h_arr,L2_err_matrix(:,5),"-v",'LineWidth',2, 'Markersize', 10)
+loglog(h_arr,L2_err_matrix(:,6),"-diamond",'LineWidth',2, 'Markersize', 10)
+loglog(h_arr,L2_err_matrix(:,7),"-square",'LineWidth',2, 'Markersize', 10)
+loglog(h_arr,L2_err_matrix(:,8),"-^",'LineWidth',2, 'Markersize', 10)
 
+temp = 0.00005 * h_arr;
+
+loglog(h_arr,temp,"--",'LineWidth',2)
 
 title('$L^{2}$ Fehler','FontSize',fontSize)
 xlabel('$h$','FontSize',fontSize)
 ylabel('$L^{2}$-Error','FontSize',fontSize)
+ylim([10^(-8),1])
+legend('$\tau = 0.1$','$\tau = 0.05$','$\tau = 0.025$','$\tau = 0.0125$','$\tau = 0.00625$','$\tau = 0.003125$','$\tau = 0.0015625$','$\tau = 0.00078125$', '$\mathcal(O)(h)$','Location','southeast','FontSize',12,'NumColumns',2)
 hold off
 
-subplot(1,2,2)
-loglog(h_arr,H1_err_matrix(:,1),"-o",'LineWidth',2)
+subplot(2,2,2)
+loglog(h_arr,H1_err_matrix(:,1),"-o",'LineWidth',2, 'Markersize', 10)
 hold on
-loglog(h_arr,H1_err_matrix(:,2),"-*",'LineWidth',2)
-loglog(h_arr,H1_err_matrix(:,3),"-+",'LineWidth',2)
-loglog(h_arr,H1_err_matrix(:,4),"-x",'LineWidth',2)
-loglog(h_arr,H1_err_matrix(:,5),"-.",'LineWidth',2)
-loglog(h_arr,H1_err_matrix(:,6),"-diamond",'LineWidth',2)
-loglog(h_arr,H1_err_matrix(:,7),"-square",'LineWidth',2)
-loglog(h_arr,H1_err_matrix(:,8),"-^",'LineWidth',2)
+loglog(h_arr,H1_err_matrix(:,2),"-*",'LineWidth',2, 'Markersize', 10)
+loglog(h_arr,H1_err_matrix(:,3),"-+",'LineWidth',2, 'Markersize', 10)
+loglog(h_arr,H1_err_matrix(:,4),"-x",'LineWidth',2, 'Markersize', 10)
+loglog(h_arr,H1_err_matrix(:,5),"-v",'LineWidth',2, 'Markersize', 10)
+loglog(h_arr,H1_err_matrix(:,6),"-diamond",'LineWidth',2, 'Markersize', 10)
+loglog(h_arr,H1_err_matrix(:,7),"-square",'LineWidth',2, 'Markersize', 10)
+loglog(h_arr,H1_err_matrix(:,8),"-^",'LineWidth',2, 'Markersize', 10)
 
+temp = 0.0005 * h_arr;
+
+loglog(h_arr,temp,"--",'LineWidth',2)
 
 title('$H^{1}$ Fehler','FontSize',fontSize)
 xlabel('$h$','FontSize',fontSize)
 ylabel('$H^{1}$-Error','FontSize',fontSize)
+ylim([10^(-8),1])
+legend('$\tau = 0.1$','$\tau = 0.05$','$\tau = 0.025$','$\tau = 0.0125$','$\tau = 0.00625$','$\tau = 0.003125$','$\tau = 0.0015625$','$\tau = 0.00078125$', '$\mathcal(O)(h)$','Location','southeast','FontSize',12,'NumColumns',2)
+
 hold off
-%}
 
 %% Plotting time
-%
 
 loyolagreen = 1/255*[0,104,87];
 deepcarrotorange = [0.9100, 0.4100, 0.1700];
@@ -137,33 +144,42 @@ set(0, 'defaulttextInterpreter','latex')
 set(0, 'defaultAxesTickLabelInterpreter','latex'); 
 set(0, 'defaultLegendInterpreter','latex');
 
-figure(2) 
-subplot(1,2,1)
-loglog(tau_arr,L2_err_matrix(1,:),"-o",'LineWidth',2)
+subplot(2,2,3)
+loglog(tau_arr,L2_err_matrix(1,:),"-o",'LineWidth',2, 'Markersize', 10)
 hold on
-loglog(tau_arr,L2_err_matrix(2,:),"-*",'LineWidth',2)
-loglog(tau_arr,L2_err_matrix(3,:),"-+",'LineWidth',2)
-loglog(tau_arr,L2_err_matrix(4,:),"-x",'LineWidth',2)
-loglog(tau_arr,L2_err_matrix(5,:),"-.",'LineWidth',2)
+loglog(tau_arr,L2_err_matrix(2,:),"-*",'LineWidth',2, 'Markersize', 10)
+loglog(tau_arr,L2_err_matrix(3,:),"-+",'LineWidth',2, 'Markersize', 10)
+loglog(tau_arr,L2_err_matrix(4,:),"-x",'LineWidth',2, 'Markersize', 10)
+loglog(tau_arr,L2_err_matrix(5,:),"-diamond",'LineWidth',2, 'Markersize', 10)
+loglog(tau_arr,L2_err_matrix(6,:),"-square",'LineWidth',2, 'Markersize', 10)
 
+temp = 0.0005 * tau_arr;
+
+loglog(tau_arr,temp,"--",'LineWidth',2)
 
 title('$L^{2}$ Fehler','FontSize',fontSize)
 xlabel('$\tau$','FontSize',fontSize)
 ylabel('$L^{2}$-Error','FontSize',fontSize)
+ylim([10^(-8),1])
+legend('dof 131','dof 362','dof 737','dof 1452','dof 2955','dof 5809', '$\mathcal(O)(\tau)$','Location','southeast','FontSize',12,'NumColumns',2)
 hold off
 
-subplot(1,2,2)
-loglog(tau_arr,H1_err_matrix(1,:),"-o",'LineWidth',2)
+subplot(2,2,4)
+loglog(tau_arr,H1_err_matrix(1,:),"-o",'LineWidth',2, 'Markersize', 10)
 hold on
-loglog(tau_arr,H1_err_matrix(2,:),"-*",'LineWidth',2)
-loglog(tau_arr,H1_err_matrix(3,:),"-+",'LineWidth',2)
-loglog(tau_arr,H1_err_matrix(4,:),"-x",'LineWidth',2)
-loglog(tau_arr,H1_err_matrix(5,:),"-.",'LineWidth',2)
+loglog(tau_arr,H1_err_matrix(2,:),"-*",'LineWidth',2, 'Markersize', 10)
+loglog(tau_arr,H1_err_matrix(3,:),"-+",'LineWidth',2, 'Markersize', 10)
+loglog(tau_arr,H1_err_matrix(4,:),"-x",'LineWidth',2, 'Markersize', 10)
+loglog(tau_arr,H1_err_matrix(5,:),"-diamond",'LineWidth',2, 'Markersize', 10)
+loglog(tau_arr,H1_err_matrix(6,:),"-square",'LineWidth',2, 'Markersize', 10)
 
+temp = 0.003 * tau_arr;
+
+loglog(tau_arr,temp,"--",'LineWidth',2)
 
 title('$H^{1}$ Fehler','FontSize',fontSize)
 xlabel('$\tau$','FontSize',fontSize)
 ylabel('$H^{1}$-Error','FontSize',fontSize)
+ylim([10^(-8),1])
+legend('dof 131','dof 362','dof 737','dof 1452','dof 2955','dof 5809', '$\mathcal(O)(\tau)$','Location','southeast','FontSize',12,'NumColumns',2)
 hold off
-%}
-toc
